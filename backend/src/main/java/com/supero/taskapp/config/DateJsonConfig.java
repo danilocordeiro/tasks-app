ab1d6da81ed53d2d98e28a6774ea18042ba37917
danilocordeiro.dev@gmail.com <danilocordeiro.dev@gmail.com>
2020-06-07 13:57:53 -0300

    Adiconado configuração de datas no json body e adicionado doc da api

---
+++ backend/src/main/java/com/supero/taskapp/config/DateJsonConfig.java
@@ -0,0 +1,28 @@
+package com.supero.taskapp.config;
+
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.format.DateTimeFormatter;
+
+@Configuration
+public class DateJsonConfig {
+    private static final String dateFormat = "dd/MM/yyyy";
+    private static final String dateTimeFormat = "dd/MM/yyyy HH:mm:ss";
+
+    @Bean
+    public Jackson2ObjectMapperBuilderCustomizer jsonCustomizer() {
+        return builder -> {
+            builder.simpleDateFormat(dateTimeFormat);
+            builder.serializers(new LocalDateSerializer(DateTimeFormatter.ofPattern(dateFormat)));
+            builder.serializers(new LocalDateTimeSerializer(DateTimeFormatter.ofPattern(dateTimeFormat)));
+            builder.deserializers(new LocalDateDeserializer(DateTimeFormatter.ofPattern(dateFormat)));
+            builder.deserializers(new LocalDateTimeDeserializer(DateTimeFormatter.ofPattern(dateTimeFormat)));
+        };
+    }
+}
--- backend/src/main/java/com/supero/taskapp/controller/TaskController.java
+++ backend/src/main/java/com/supero/taskapp/controller/TaskController.java
@@ -2,6 +2,11 @@ package com.supero.taskapp.controller;
 
 import com.supero.taskapp.entities.Task;
 import com.supero.taskapp.service.TaskService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -20,6 +25,9 @@ public class TaskController {
     }
 
 
+    @Operation(summary = "Get all tasks")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Return a list of tasks")})
     @GetMapping()
     public ResponseEntity<List<Task>> findAllTasks() {
 
@@ -27,7 +35,13 @@ public class TaskController {
 
     }
 
-
+    @Operation(summary = "Get task by id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Found the task",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Task.class)) }),
+            @ApiResponse(responseCode = "404", description = "Task not found",
+                    content = @Content) })
     @GetMapping("/{id}")
     public ResponseEntity<Task> findById(@PathVariable Long id) {
 
@@ -36,6 +50,13 @@ public class TaskController {
         return  ResponseEntity.ok(task);
     }
 
+    @Operation(summary = "Update task")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "task updated",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Task.class)) }),
+            @ApiResponse(responseCode = "404", description = "Task not found",
+                    content = @Content) })
     @PutMapping("/{id}")
     public ResponseEntity<Task> update(@PathVariable Long id, @Valid @RequestBody Task task) {
         Task taskUpdated = this.taskService.update(id, task);
@@ -43,12 +64,23 @@ public class TaskController {
         return  ResponseEntity.ok(taskUpdated);
     }
 
+    @Operation(summary = "done/undone task")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "success done/undone task",
+                    content = @Content),
+            @ApiResponse(responseCode = "404", description = "Task not found",
+                    content = @Content) })
     @PutMapping("/{id}/completed")
     @ResponseStatus(HttpStatus.OK)
     public void completedTask(@PathVariable Long id) {
         taskService.updateCompleted(id);
     }
 
+    @Operation(summary = "create task")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "task created",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Task.class)) }) })
     @PostMapping
     public ResponseEntity<Task> create(@Valid @RequestBody Task task) {
 
@@ -56,6 +88,12 @@ public class TaskController {
     }
 
 
+    @Operation(summary = "delete task")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "success delete task",
+                    content = @Content),
+            @ApiResponse(responseCode = "404", description = "Task not found",
+                    content = @Content) })
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void remover(@PathVariable Long id) {
