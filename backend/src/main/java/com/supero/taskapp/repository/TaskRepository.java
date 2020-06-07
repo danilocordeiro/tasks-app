130073c07380e4e52875ce49940e7d3099852510
danilocordeiro.dev@gmail.com <danilocordeiro.dev@gmail.com>
2020-06-07 12:51:20 -0300

    criado o task repository

---
+++ backend/src/main/java/com/supero/taskapp/repository/TaskRepository.java
@@ -0,0 +1,9 @@
+package com.supero.taskapp.repository;
+
+import com.supero.taskapp.entities.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TaskRepository extends JpaRepository<Task, Long> {
+}
